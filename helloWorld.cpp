7963bc6290bec7ec5bfcbd2df7efadd1baf8463e
Kevin Pham <67347904+phamkevin04@users.noreply.github.com>
2021-05-07 10:32:09 -0700

    created sample file
    set-up of c++ on macos

---
+++ helloWorld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+    int numInput;
+
+    cout << "Hello World" << endl;
+    
+    cout << "Input number: ";
+    cin >> numInput;
+
+    return 0;
+}
